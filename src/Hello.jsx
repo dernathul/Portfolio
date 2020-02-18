d9371cc69c111e44691f46c8272dfeb43c69adc6
dernathul <lautaroparracab@gmail.com>
2020-02-18 15:39:32 +0100

    trying to solve security issues

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -4,8 +4,9 @@ const Hello = () => {
     return (
     <div className="ui main container">
         <h1 id="hello"> FullStack Developer</h1>
+        <img src= "src/images/fullstack.jpeg"></img>
         </div>
     );
-};
+}
 
 export default Hello;
