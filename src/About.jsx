2d9d29c9e6e87b5737e3354afca111cb36a9e664
dernathul <lautaroparracab@gmail.com>
2020-02-19 15:48:02 +0100

    adding image to about

--- src/About.jsx
+++ src/About.jsx
@@ -13,10 +13,16 @@ const About = () => {
           <p>
           I am a father of two boys, who has learned multiple skills across life. I have lived in several cities in different countries, falling in love with Sweden since i first came in 2016.
           I have learned programming by a fullstack developer Bootcamp in Craft Academy, in Göteborgs. I love to code and its applications, but i also love to learn more about this ever changing field.
-          </p>
+          </p> 
+          <div id="lautarox">
+        <img src="src/images/lautaro.jpg" />
+       </div>     
         </div>
       </div>
     </div>
+       
+    
+    
 
   );
 };
