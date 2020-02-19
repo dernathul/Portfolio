3c76e67295a3727c358e4a50dd9d92fa2096c6ea
dernathul <lautaroparracab@gmail.com>
2020-02-19 16:44:27 +0100

    image responsive

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -4,7 +4,9 @@ const Hello = () => {
     return (
     <div className="ui main container">
         <h1 id="hello"> FullStack Developer</h1>
+        <div id="fullstack"class="responsive">
         <img src= "src/images/fullstack.jpeg"></img>
+        </div>
         </div>
     );
 }
