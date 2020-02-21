143d034c967884d446fb8656bd43b9dbdd6cde80
dernathul <lautaroparracab@gmail.com>
2020-02-21 11:48:53 +0100

    error corrected

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -4,7 +4,7 @@ const Hello = () => {
     return (
     <div className="ui main container">
         <h1 id="hello"> A man's friendships are one of the best measures of his worth.</h1>
-        <h2>Charles Darwin</h2>
+        <h1> A man's friendships are one of the best measures of his worth.Charles Darwin</h1>
         <div id="fullstack"class="responsive">
         <img src= "src/images/group.jpg"></img>
         </div>
