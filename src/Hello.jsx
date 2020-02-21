7aa4b69061d1b67131b7a5232456354e4e670ad4
dernathul <lautaroparracab@gmail.com>
2020-02-21 11:49:59 +0100

    message

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hello = () => {
     return (
     <div className="ui main container">
-        <h1 id="hello"> A man's friendships are one of the best measures of his worth.</h1>
+        <h1 id="hello"> Lautaro Parra C</h1>
         <h1> A man's friendships are one of the best measures of his worth.Charles Darwin</h1>
         <div id="fullstack"class="responsive">
         <img src= "src/images/group.jpg"></img>
