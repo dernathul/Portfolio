f3de6f5a1497b1f63650171105c764a3a8c6cbc0
dernathul <lautaroparracab@gmail.com>
2020-03-12 14:48:08 +0100

    fixed format

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,79 +1,90 @@
 import React from "react";
-import {UndrawEmailCapture} from "react-undraw-illustrations";
+import { UndrawEmailCapture } from "react-undraw-illustrations";
 
-
-
-
-const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-        
-       
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", company: "", email: "", message: "" };
   }
-  class ContactForm extends React.Component {
-    constructor(props) {
-        
-      super(props);
-      this.state = { name: "",company: "", email: "", message: "" };
-    }
 
-    handleSubmit = e => {
-      fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...this.state })
-      })
-        .then(() => alert("Success!"))
-        .catch(error => alert(error));
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
 
-      e.preventDefault();
-    };
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
-    
-    render() {
-       
-      const { name,  company, email, message } = this.state;
-      
-      return (
-        
-          <div className="ui container">
+    e.preventDefault();
+  };
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, company, email, message } = this.state;
+
+    return (
+      <div className="ui container">
         <form className="ui form" onSubmit={this.handleSubmit}>
-          <div class='mail'>
-        <UndrawEmailCapture />
-        </div>
+          <div class="mail">
+            <UndrawEmailCapture />
+          </div>
           <div className="form">
             <label>
-            Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+              Name:{" "}
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.handleChange}
+              />
             </label>
-            
           </div>
           <p>
             <label>
-            Company: <input type="text" name="company" value={company} onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label >
-            Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+              Company:{" "}
+              <input
+                type="text"
+                name="company"
+                value={company}
+                onChange={this.handleChange}
+              />
             </label>
           </p>
           <p>
             <label>
-            Message: <textarea name="message" value={message} onChange={this.handleChange} />
+              Email:{" "}
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.handleChange}
+              />
             </label>
           </p>
-          
+          <p>
+            <label>
+              Message:{" "}
+              <textarea
+                name="message"
+                value={message}
+                onChange={this.handleChange}
+              />
+            </label>
+          </p>
+
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
-export default ContactForm
-
-
-
+export default ContactForm;
